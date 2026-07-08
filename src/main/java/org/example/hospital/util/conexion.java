32ff829419ef8c82e8d7c1b179060973bd938ca4
kevinmendoza2001 <shaka123kamm@gmail.com>
2026-07-08 13:02:01 -0500

    CommitKmendoza

--- src/main/java/org/example/hospital/util/conexion.java
+++ src/main/java/org/example/hospital/util/conexion.java
@@ -1,4 +1,16 @@
 package org.example.hospital.util;
 
-public class conexion {
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class Conexion {
+
+    private static final String URL = "jdbc:oracle:thin:@localhost:1521:XE";
+    private static final String USUARIO = "system";
+    private static final String CLAVE = "tu_contraseña";
+
+    public static Connection conectar() throws SQLException {
+        return DriverManager.getConnection(URL, USUARIO, CLAVE);
+    }
 }
